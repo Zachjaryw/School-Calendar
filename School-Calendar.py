213a08e4049ac35a4c7065bb36115784646a9250
Zachjaryw <85905928+Zachjaryw@users.noreply.github.com>
2022-01-27 19:12:08 -0800

    Add files via upload

--- School-Calendar.py
+++ School-Calendar.py
@@ -307,7 +307,7 @@ user = 'Zach'
 year = st.selectbox('Year:',[2021,2022,2023])
 semester = st.selectbox('Semester:',['Spring','Fall'])
 filename = f'/{user}SchoolCalendar {semester} {year}.json'
-if password == "bella":
+if password == "password":
     dbx = initialize()
     calendar = fromDBX(dbx,filename)
     Action = st.selectbox("Select Action",["Assignments Due This Week", "Progress", "Adjust Assignment", "New Assignment", "Show Old Assignments", "Assignments Due This Month", "Show Assignments by Type", "Show Full Calendar","Review Single Assignment","Add Assignments from file","Assignments In Date Range"])
