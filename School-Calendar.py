b971190ed878090d468dc19b05ba8daadbc7a971
Zachjaryw <85905928+Zachjaryw@users.noreply.github.com>
2022-01-27 18:37:16 -0800

    Add files via upload

--- School-Calendar.py
+++ School-Calendar.py
@@ -298,13 +298,13 @@ def completeAction(Action):
   elif Action == "***SELECT ACTION***":
     st.text("Please select an action")
 
-user = st.text_input('Username:','Zach')
+password = st.text_input('Password:',"*")
+user = 'Zach'
 year = st.selectbox('Year:',[2021,2022,2023])
 semester = st.selectbox('Semester:',['Spring','Fall'])
 filename = f'/{user}SchoolCalendar {semester} {year}.json'
-
-dbx = initialize()
-calendar = fromDBX(dbx,filename)
-
-Action = st.selectbox("Select Action",["Assignments Due This Week", "Progress", "Adjust Assignment", "New Assignment", "Show Old Assignments", "Assignments Due This Month", "Show Assignments by Type", "Show Full Calendar","Review Single Assignment","Add Assignments from file","Assignments In Date Range"])
-completeAction(Action)
+if password == "bella":
+    dbx = initialize()
+    calendar = fromDBX(dbx,filename)
+    Action = st.selectbox("Select Action",["Assignments Due This Week", "Progress", "Adjust Assignment", "New Assignment", "Show Old Assignments", "Assignments Due This Month", "Show Assignments by Type", "Show Full Calendar","Review Single Assignment","Add Assignments from file","Assignments In Date Range"])
+    completeAction(Action)
