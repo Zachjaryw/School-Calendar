0a2a347fc1ffb8b9a69330368fbb0b3decaa0b3c
Zachjaryw <85905928+Zachjaryw@users.noreply.github.com>
2022-02-05 19:10:26 -0800

    Add files via upload

--- School-Calendar.py
+++ School-Calendar.py
@@ -317,5 +317,5 @@ if password == acceptPassword:
     Action = st.selectbox("Select Action",["Assignments Due This Week", "Progress", "Adjust Assignment", "New Assignment", "Show Old Assignments", "Assignments Due This Month", "Show Assignments by Type", "Show Full Calendar","Review Single Assignment","Add Assignments from file","Assignments In Date Range"])
     completeAction(Action)
 elif password == "SET TO NEW CALENDAR MODE":
-  calendar = {'Zach':'bellabarry',calendar}
+  calendar = {'Zach':['bellabarry',calendar]}
   save_cal()
