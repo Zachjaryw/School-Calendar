c247aa7e3c2df5cdabd47aa1f1a47c4accd5cfc1
Zachjaryw <85905928+Zachjaryw@users.noreply.github.com>
2022-03-28 16:14:17 -0700

    Update School-Calendar.py

--- School-Calendar.py
+++ School-Calendar.py
@@ -7,7 +7,7 @@ from Dropbox_Setup import * #access dropbox
 from Send_Message import * #access message sending
 
 st.set_page_config(layout="wide")
-st.header("School Calendar")
+st.title("School Calendar")
 
 
 def setup_new_semester():
