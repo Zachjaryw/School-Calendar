d746eed3432389bdf67705c1b3b19758bf5ae51b
Zachjaryw <85905928+Zachjaryw@users.noreply.github.com>
2022-06-21 10:21:24 -0700

    Update School-Calendar.py

--- School-Calendar.py
+++ School-Calendar.py
@@ -488,10 +488,10 @@ if user != 'NEW' and user in decrypted:
         semester = st.selectbox('Semester:',semesters)
       try:
         calendar = fromDBX(dbx,f'{st.secrets.file.studentAccess}{acceptUser}/{semester} {year}.json')
+        Action = st.selectbox("Select Action",["Assignments Due This Week", "New Assignment", "Adjust Assignment", "Show Old Assignments","Assignments In Date Range","Course Assignments","My Courses"])     #["Assignments Due This Week", "Progress", "Adjust Assignment", "New Assignment", "Show Old Assignments", "Assignments Due This Month", "Show Assignments by Type", "Show Full Calendar","Review Single Assignment","Add Assignments from file","Assignments In Date Range"])
+        completeAction(Action)
       except:
         st.warning('This Year does not exist for the selected user.')
-      Action = st.selectbox("Select Action",["Assignments Due This Week", "New Assignment", "Adjust Assignment", "Show Old Assignments","Assignments In Date Range","Course Assignments","My Courses"])     #["Assignments Due This Week", "Progress", "Adjust Assignment", "New Assignment", "Show Old Assignments", "Assignments Due This Month", "Show Assignments by Type", "Show Full Calendar","Review Single Assignment","Add Assignments from file","Assignments In Date Range"])
-      completeAction(Action)
 elif user == "NEW":
   authorization = st.text_input('Please type the authorization code here:',"access-")
   if st.button('Press to generate access code'):
