ea92a58923318eb101031e031669385d536e1f40
Zachjaryw <85905928+Zachjaryw@users.noreply.github.com>
2022-06-21 10:20:34 -0700

    Update School-Calendar.py

--- School-Calendar.py
+++ School-Calendar.py
@@ -488,7 +488,7 @@ if user != 'NEW' and user in decrypted:
         semester = st.selectbox('Semester:',semesters)
       try:
         calendar = fromDBX(dbx,f'{st.secrets.file.studentAccess}{acceptUser}/{semester} {year}.json')
-       except:
+      except:
         st.warning('This Year does not exist for the selected user.')
       Action = st.selectbox("Select Action",["Assignments Due This Week", "New Assignment", "Adjust Assignment", "Show Old Assignments","Assignments In Date Range","Course Assignments","My Courses"])     #["Assignments Due This Week", "Progress", "Adjust Assignment", "New Assignment", "Show Old Assignments", "Assignments Due This Month", "Show Assignments by Type", "Show Full Calendar","Review Single Assignment","Add Assignments from file","Assignments In Date Range"])
       completeAction(Action)
