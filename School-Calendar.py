4d15e400a819afdc4603312897d42fc18fd319fb
Zachary Werner <85905928+Zachjaryw@users.noreply.github.com>
2023-08-02 09:43:43 -0500

    Update School-Calendar.py

--- School-Calendar.py
+++ School-Calendar.py
@@ -612,7 +612,7 @@ elif user == "NEW":
         st.text(f'New account for {newUsername} has been activated. \nChange username field at the top of the screen to begin.')
   else:
     st.text('Please Enter Auth Key from Developer')
-elif user == "HELP":
+elif user == "Help":
   showHelp()
 elif user not in decrypted:
   st.warning("Enter Valid Username")
